f7e150d63378779f2566a56869896c07aece69a6
ronnahom96 <ronnahom96@gmail.com>
2023-03-23 16:18:00 +0200

    remove row and cols from generic elements

--- src/components/genericElements/SelectInput/SelectInput.js
+++ src/components/genericElements/SelectInput/SelectInput.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function SelectInput({ row, col, label, value }) {
+export default function SelectInput({ label, value }) {
     return (
         <div className='select-container'>
             <h3 className='label'>{label}</h3>
--- src/components/genericElements/textInput/TextInput.js
+++ src/components/genericElements/textInput/TextInput.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function TextInput({ row, col, label, value }) {
+export default function TextInput({ label, value }) {
     return (
         <div className='text-input-container'>
             <h3 className='label'>{label}</h3>
